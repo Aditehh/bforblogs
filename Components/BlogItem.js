a38c4ba6ffaa8b9200f702dc926bed17a5ed3465
aditehh <aditya.aryal888@gmail.com>
2025-09-24 20:26:24 +0545

    this

--- Components/BlogItem.js
+++ Components/BlogItem.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { assets, blog_data } from '@/Assets/Nextjs-blog-assets/Assets/assets'
 
-const BlogItem = ({title, description, category, image}) => {
+const BlogItem = ({title, description, category, image, id}) => {
     return (
         <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000'>
+            <Link href={`/blogs/${id}`}>
             <Image src={image} alt='no' width={400} height={400} className='border' />
-
+           </Link>
             <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm '>
                 {category}
             </p>
@@ -18,9 +20,9 @@ const BlogItem = ({title, description, category, image}) => {
                 <p className='mb-3 text-sm tracking-tight text-gray-700'>
                     {description}
                 </p>
-                <div className='inline-flex items-center py-2 font-semibold text-center'>
+                <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center'>
                     Read more <Image className='ml-2' alt='f' width={12} src={assets.arrow} />
-                </div>
+                </Link>
             </div>
         </div>
     )
